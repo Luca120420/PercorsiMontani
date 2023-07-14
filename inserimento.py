f50c19b4ad74624cefbea6b952cb5a2869631894
Stefano Carobene <stefano.carobene04@gmail.com>
2023-07-14 11:15:53 +0200

    Ricerca path o paths per andare da rif. x a rif. y

--- inserimento.py
+++ inserimento.py
@@ -1,110 +1,99 @@
 from neo4j import GraphDatabase
 
-# Crea la connessione al database Neo4j
 url = "neo4j+s://793c71a3.databases.neo4j.io"
 username = "neo4j"
 password = "E_OchQ6w3qMp6jPMSVd9rYJVjczHKUa-sRqy_smT8u8" 
 driver = GraphDatabase.driver(url, auth=(username, password))
 
-# Funzione per creare la zona di montagna nel database
-def create_mountain_zone(tx, name):
-    tx.run("CREATE (:MountainZone {name: $name})", name=name)
+class SentieriMontani:
 
-# Funzione per creare un sentiero e collegarlo alla zona di montagna corrispondente
-def create_trail(tx, name, mountain_zone, suitable_for_bike, duration, difficulty, starting_point):
-    tx.run("""
-        MATCH (m:MountainZone {name: $mountain_zone})
-        CREATE (m)-[:HAS_TRAIL]->(:Trail {name: $name, suitable_for_bike: $suitable_for_bike, duration: $duration, difficulty: $difficulty, starting_point: $starting_point})
-    """, name=name, mountain_zone=mountain_zone, suitable_for_bike=suitable_for_bike, duration=duration, difficulty=difficulty, starting_point=starting_point)
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
-# Funzione per creare una relazione di arrivo tra un sentiero e un rifugio
-def create_arrival(tx, trail, refuge):
-    tx.run("""
-        MATCH (t:Trail {name: $trail}), (r:Refuge {name: $refuge})
-        CREATE (t)-[:ARRIVES_AT]->(r)
-    """, trail=trail, refuge=refuge)
+    def close(self):
+        self.driver.close()
 
-# Funzione per creare una relazione di collegamento tra due sentieri
-def create_leads_to(tx, trail1, trail2):
-    tx.run("""
-        MATCH (t1:Trail {name: $trail1}), (t2:Trail {name: $trail2})
-        CREATE (t1)-[:LEADS_TO]->(t2)
-    """, trail1=trail1, trail2=trail2)
-def create_refuge(tx, name, trails):
-    tx.run("""
-        MATCH (t:Trail)
-        WHERE t.name IN $trails
-        CREATE (t)-[:LEADS_TO]->(:Refuge {name: $name})
-    """, name=name, trails=trails)
+    @staticmethod
+    def create_mountain_zone(tx, name):
+        tx.run("CREATE (:MountainZone {name: $name})", name=name)
 
-# Crea la zona di montagna, i sentieri e i rifugi nel database
-def create_mountain_database():
-    with driver.session() as session:
-        session.write_transaction(create_mountain_zone, "Montagna A")
+    @staticmethod
+    def create_trail(tx, name, mountain_zone, suitable_for_bike, duration, difficulty, starting_point):
+        tx.run("""
+            MATCH (m:MountainZone {name: $mountain_zone})
+            CREATE (m)-[:HAS_TRAIL]->(:Trail {name: $name, suitable_for_bike: $suitable_for_bike, duration: $duration, difficulty: $difficulty, starting_point: $starting_point})
+        """, name=name, mountain_zone=mountain_zone, suitable_for_bike=suitable_for_bike, duration=duration,
+               difficulty=difficulty, starting_point=starting_point)
 
-        # Crea i sentieri
-        session.write_transaction(create_trail, "Sentiero 1", "Montagna A", True, 120, "Facile", "Punto A")
-        session.write_transaction(create_trail, "Sentiero 2", "Montagna A", False, 90, "Medio", "Punto B")
-        session.write_transaction(create_trail, "Sentiero 3", "Montagna A", True, 180, "Difficile", "Punto C")
-        session.write_transaction(create_trail, "Sentiero 4", "Montagna A", True, 150, "Medio", "Punto D")
-        session.write_transaction(create_trail, "Sentiero 5", "Montagna A", False, 120, "Difficile", "Punto E")
-        # Aggiungi altri sentieri qui...
+    @staticmethod
+    def create_arrival(tx, trail, refuge):
+        tx.run("""
+            MATCH (t:Trail {name: $trail}), (r:Refuge {name: $refuge})
+            CREATE (t)-[:ARRIVES_AT]->(r)
+        """, trail=trail, refuge=refuge)
 
-        # Crea i rifugi
-        session.write_transaction(create_refuge, "Rifugio 1", ["Sentiero 1", "Sentiero 2"])
-        session.write_transaction(create_refuge, "Rifugio 2", ["Sentiero 2", "Sentiero 3"])
-        session.write_transaction(create_refuge, "Rifugio 3", ["Sentiero 3", "Sentiero 4"])
-        session.write_transaction(create_refuge, "Rifugio 4", ["Sentiero 4", "Sentiero 5"])
-        session.write_transaction(create_refuge, "Rifugio 5", ["Sentiero 5", "Sentiero 1"])
-        # Aggiungi altri rifugi qui...
+    @staticmethod
+    def create_leads_to(tx, trail1, trail2):
+        tx.run("""
+            MATCH (t1:Trail {name: $trail1}), (t2:Trail {name: $trail2})
+            CREATE (t1)-[:LEADS_TO]->(t2)
+        """, trail1=trail1, trail2=trail2)
 
-        # Crea le relazioni di arrivo ai rifugi
-        session.write_transaction(create_arrival, "Sentiero 1", "Rifugio 1")
-        session.write_transaction(create_arrival, "Sentiero 2", "Rifugio 1")
-        session.write_transaction(create_arrival, "Sentiero 2", "Rifugio 2")
-        session.write_transaction(create_arrival, "Sentiero 3", "Rifugio 2")
-        session.write_transaction(create_arrival, "Sentiero 3", "Rifugio 3")
-        session.write_transaction(create_arrival, "Sentiero 4", "Rifugio 3")
-        session.write_transaction(create_arrival, "Sentiero 4", "Rifugio 4")
-        session.write_transaction(create_arrival, "Sentiero 5", "Rifugio 4")
-        session.write_transaction(create_arrival, "Sentiero 5", "Rifugio 5")
-        session.write_transaction(create_arrival, "Sentiero 1", "Rifugio 5")
-        # Aggiungi altre relazioni di arrivo qui...
+    @staticmethod
+    def create_refuge(tx, name, trails):
+        tx.run("""
+            MATCH (t:Trail)
+            WHERE t.name IN $trails
+            CREATE (t)-[:LEADS_TO]->(:Refuge {name: $name})
+        """, name=name, trails=trails)
 
-        # Crea le relazioni di collegamento tra sentieri
-        session.write_transaction(create_leads_to, "Sentiero 1", "Sentiero 2")
-        session.write_transaction(create_leads_to, "Sentiero 2", "Sentiero 3")
-        session.write_transaction(create_leads_to, "Sentiero 3", "Sentiero 4")
-        session.write_transaction(create_leads_to, "Sentiero 4", "Sentiero 5")
-        session.write_transaction(create_leads_to, "Sentiero 5", "Sentiero 1")
-        # Aggiungi altre relazioni di collegamento qui...
+    def create_mountain_database(self):
+        with self.driver.session() as session:
+            session.write_transaction(self.create_mountain_zone, "Montagna A")
 
-        # Aggiungi altri sentieri e relazioni
-        session.write_transaction(create_trail, "Sentiero 6", "Montagna A", True, 120, "Facile", "Punto F")
-        session.write_transaction(create_trail, "Sentiero 7", "Montagna A", False, 90, "Medio", "Punto G")
-        session.write_transaction(create_trail, "Sentiero 8", "Montagna A", True, 180, "Difficile", "Punto H")
-        session.write_transaction(create_trail, "Sentiero 9", "Montagna A", True, 150, "Medio", "Punto I")
-        session.write_transaction(create_trail, "Sentiero 10", "Montagna A", False, 120, "Difficile", "Punto J")
-        # Aggiungi altri sentieri qui...
+            # Crea i sentieri
+            session.write_transaction(self.create_trail, "Sentiero 1", "Montagna A", True, 120, "Facile", "Punto A")
+            session.write_transaction(self.create_trail, "Sentiero 2", "Montagna A", False, 90, "Medio", "Punto B")
+            session.write_transaction(self.create_trail, "Sentiero 3", "Montagna A", True, 180, "Difficile", "Punto C")
+            session.write_transaction(self.create_trail, "Sentiero 4", "Montagna A", True, 150, "Medio", "Punto D")
+            session.write_transaction(self.create_trail, "Sentiero 5", "Montagna A", False, 120, "Difficile", "Punto E")
 
-        # Crea le nuove relazioni di arrivo ai rifugi
-        session.write_transaction(create_arrival, "Sentiero 6", "Rifugio 1")
-        session.write_transaction(create_arrival, "Sentiero 7", "Rifugio 2")
-        session.write_transaction(create_arrival, "Sentiero 8", "Rifugio 3")
-        session.write_transaction(create_arrival, "Sentiero 9", "Rifugio 4")
-        session.write_transaction(create_arrival, "Sentiero 10", "Rifugio 5")
-        # Aggiungi altre relazioni di arrivo qui...
+            # Crea i rifugi
+            session.write_transaction(self.create_refuge, "Rifugio 1", ["Sentiero 1", "Sentiero 2"])
+            session.write_transaction(self.create_refuge, "Rifugio 2", ["Sentiero 2", "Sentiero 3"])
+            session.write_transaction(self.create_refuge, "Rifugio 3", ["Sentiero 3", "Sentiero 4"])
+            session.write_transaction(self.create_refuge, "Rifugio 4", ["Sentiero 4", "Sentiero 5"])
+            session.write_transaction(self.create_refuge, "Rifugio 5", ["Sentiero 5", "Sentiero 1"])
 
-        # Crea le nuove relazioni di collegamento tra sentieri
-        session.write_transaction(create_leads_to, "Sentiero 6", "Sentiero 7")
-        session.write_transaction(create_leads_to, "Sentiero 7", "Sentiero 8")
-        session.write_transaction(create_leads_to, "Sentiero 8", "Sentiero 9")
-        session.write_transaction(create_leads_to, "Sentiero 9", "Sentiero 10")
-        session.write_transaction(create_leads_to, "Sentiero 10", "Sentiero 6")
-        # Aggiungi altre relazioni di collegamento qui...
+            # Crea le relazioni di arrivo ai rifugi
+            session.write_transaction(self.create_arrival, "Sentiero 1", "Rifugio 1")
+            session.write_transaction(self.create_arrival, "Sentiero 2", "Rifugio 1")
+            session.write_transaction(self.create_arrival, "Sentiero 2", "Rifugio 2")
+            session.write_transaction(self.create_arrival, "Sentiero 3", "Rifugio 2")
+            session.write_transaction(self.create_arrival, "Sentiero 3", "Rifugio 3")
+            session.write_transaction(self.create_arrival, "Sentiero 4", "Rifugio 3")
+            session.write_transaction(self.create_arrival, "Sentiero 4", "Rifugio 4")
+            session.write_transaction(self.create_arrival, "Sentiero 5", "Rifugio 4")
+            session.write_transaction(self.create_arrival, "Sentiero 5", "Rifugio 5")
+            session.write_transaction(self.create_arrival, "Sentiero 1", "Rifugio 5")
 
-# Esegui la creazione del database
-create_mountain_database()
+            # Crea le relazioni di collegamento tra sentieri
+            session.write_transaction(self.create_leads_to, "Sentiero 1", "Sentiero 2")
+            session.write_transaction(self.create_leads_to, "Sentiero 2", "Sentiero 3")
+            session.write_transaction(self.create_leads_to, "Sentiero 3", "Sentiero 4")
+            session.write_transaction(self.create_leads_to, "Sentiero 4", "Sentiero 5")
+            session.write_transaction(self.create_leads_to, "Sentiero 5", "Sentiero 1")
 
-# Chiudi la connessione al database
-driver.close()
+            # Aggiungi altri sentieri e relazioni
+            session.write_transaction(self.create_trail, "Sentiero 6", "Montagna A", True, 120, "Facile", "Punto F")
+            session.write_transaction(self.create_trail, "Sentiero 7", "Montagna A", False, 90, "Medio", "Punto G")
+            session.write_transaction(self.create_trail, "Sentiero 8", "Montagna A", True, 180, "Difficile", "Punto H")
+            session.write_transaction(self.create_trail, "Sentiero 9", "Montagna A", True, 150, "Medio", "Punto I")
+            session.write_transaction(self.create_trail, "Sentiero 10", "Montagna A", False, 120, "Difficile", "Punto J")
+
+
+if __name__ == "__main__":
+    greeter = SentieriMontani("neo4j+s://793c71a3.databases.neo4j.io", "neo4j",
+                              "E_OchQ6w3qMp6jPMSVd9rYJVjczHKUa-sRqy_smT8u8")
+    greeter.create_mountain_database()
+    greeter.close()
--- richieste.py
+++ richieste.py
@@ -2,71 +2,28 @@ from neo4j import GraphDatabase
 
 url = "neo4j+s://793c71a3.databases.neo4j.io"
 username = "neo4j"
-password = "E_OchQ6w3qMp6jPMSVd9rYJVjczHKUa-sRqy_smT8u8" 
+password = "E_OchQ6w3qMp6jPMSVd9rYJVjczHKUa-sRqy_smT8u8"
 driver = GraphDatabase.driver(url, auth=(username, password))
 
-# Funzione per trovare il percorso più breve tra due rifugi
-def find_shortest_path(tx, start_refuge, end_refuge, suitable_for_bike):
-    if suitable_for_bike:
-        result = tx.run("""
-            MATCH (start:Refuge {name: $start_refuge}), (end:Refuge {name: $end_refuge})
-            MATCH path = shortestPath((start)-[:ARRIVES_AT*]-(end))
-            WHERE all(rel in relationships(path) WHERE rel.suitable_for_bike = true)
-            RETURN path
-        """, start_refuge=start_refuge, end_refuge=end_refuge)
-    else:
-        result = tx.run("""
-            MATCH (start:Refuge {name: $start_refuge}), (end:Refuge {name: $end_refuge})
-            MATCH path = shortestPath((start)-[:ARRIVES_AT*]-(end))
-            RETURN path
-        """, start_refuge=start_refuge, end_refuge=end_refuge)
-    return result.single()
 
-# Funzione per ottenere i sentieri da un percorso
-def get_trails_from_path(tx, path):
-    trails = []
-    for i in range(len(path)-1):
-        result = tx.run("""
-            MATCH (s:Trail)-[:LEADS_TO]->(e:Trail)
-            WHERE s.name = $start_trail AND e.name = $end_trail
-            RETURN s.name, e.name
-        """, start_trail=path[i], end_trail=path[i+1])
-        record = result.single()
-        if record:
-            trails.append(record["s.name"])
-        else:
-            return None
-    return trails
+def find_paths_between_refuges(tx, start_refuge, end_refuge):
+      result = tx.run("""
+            MATCH (start:Refuge {name: $start_refuge}), (end:Refuge {name: $end_refuge}),
+            path = shortestPath((start)-[:LEADS_TO*]-(end))
+            RETURN [node IN nodes(path) | node.name] AS path_names
+      """, start_refuge=start_refuge, end_refuge=end_refuge)
 
-# Ottieni i sentieri per arrivare dal rifugio di partenza al rifugio di destinazione
-def get_trails_between_refuges(start_refuge, end_refuge, suitable_for_bike):
-    with driver.session() as session:
-        result = session.read_transaction(find_shortest_path, start_refuge, end_refuge, suitable_for_bike)
-        if result:
-            path = [record["name"] for record in result["path"].nodes]
-            trails = session.read_transaction(get_trails_from_path, path)
-            return trails
-        else:
-            return None
+      paths = result.single()
+      if paths:
+            paths = paths["path_names"]
+            print(f"I percorsi tra il rifugio {start_refuge} e il rifugio {end_refuge} sono:")
+            for path in paths:
+                  print(path)
+      else:
+            print(f"Non esistono percorsi tra il rifugio {start_refuge} e il rifugio {end_refuge}.")
 
-# Funzione principale del programma
-def main():
-    start_refuge = input("Inserisci il rifugio di partenza: ")
-    end_refuge = input("Inserisci il rifugio di destinazione: ")
-    suitable_for_bike = input("Sei in bici? (S/N): ").upper() == "S"
-    print(suitable_for_bike)
 
-    trails = get_trails_between_refuges(start_refuge, end_refuge, suitable_for_bike)
-    if trails:
-        print(f"\nPercorso consigliato:")
-        for i, trail in enumerate(trails, start=1):
-            print(f"{i}. {trail}")
-    else:
-        print("Sentiero assente o percorso non trovato.")
+with driver.session() as session:
+      session.read_transaction(find_paths_between_refuges, "Rifugio 1", "Rifugio 4")
 
-# Esegui il programma
-if __name__ == "__main__":
-    main()
-
-# Chiudi la connessione al database
 driver.close()
